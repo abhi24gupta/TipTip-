afc5c1744454dc644ed2cac1bdf95b4836965cac
abhi24gupta <44904063+abhi24gupta@users.noreply.github.com>
2020-04-20 00:26:14 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,158 @@
+var keydata={
+    a:{
+        color :'#ff7f50',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    b:{
+        color :'#28b4c9',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    c:{
+        color :'#f268ae',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    d:{
+        color :'#9f9892',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    e:{
+        color :'#e7f6d9',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    f:{
+        color :'#d9f5f6',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    g:{
+        color :'#415d4f',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    h:{
+        color :'#ca0f6f',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    i:{
+        color :'#ffd700',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    j:{
+        color :'#003366',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    k:{
+        color :'#008000',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    l:{
+        color :'#daa520',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    m:{
+        color :'#ff4040',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    n:{
+        color :'#ff0000',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    o:{
+        color :'#bada55',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    p:{
+        color :'#fafafa',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    q:{
+        color :'	#ff6a6a',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    r:{
+        color :'	#7cfc00',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    s:{
+        color :'	#f08080',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    t:{
+        color :'	#20b2aa',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    u:{
+        color :'	#b3ee3a',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    v:{
+        color :'#90ee90',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    w:{
+        color :'	#7d26cd',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    x:{
+        color :'	#4eee94',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    y:{
+        color :'	#ffff00',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    },
+    z:{
+        color :'#ff3e96',
+        sound :new Howl({
+            src: ['sounds/.mp3']
+          })
+    }
+}
